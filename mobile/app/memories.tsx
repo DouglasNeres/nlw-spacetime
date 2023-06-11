53b6558dd1c82a3eee74046786f8755c7f5b8750
DouglasNeres <douglasneres06@gmail.com>
2023-06-11 15:47:29 -0300

    Logout button and API search for memory layout

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -1,9 +1,110 @@
-import { Text, View } from 'react-native'
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import Icon from '@expo/vector-icons/Feather'
+import { Link, useRouter } from 'expo-router'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import * as SecureStore from 'expo-secure-store'
+import { useEffect, useState } from 'react'
+import { api } from '../src/lib/api'
+import dayjs from 'dayjs'
+import ptBr from 'dayjs/locale/pt-br'
+
+dayjs.locale(ptBr)
+
+interface MemoryProps {
+  coverUrl: string
+  excerpt: string
+  id: string
+  createdAt: string
+}
 
 export default function Memories() {
+  const { bottom, top } = useSafeAreaInsets()
+  const router = useRouter()
+  const [memories, setMemories] = useState<MemoryProps[]>([])
+
+  async function logOut() {
+    await SecureStore.deleteItemAsync('token')
+
+    router.push('/')
+  }
+
+  async function loadMemomries() {
+    const token = await SecureStore.getItemAsync('token')
+
+    const response = await api.get('/memories', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    setMemories(response.data)
+  }
+
+  useEffect(() => {
+    loadMemomries()
+  }, [])
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Memories</Text>
-    </View>
+    <ScrollView
+      className="flex-1 px-8"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="mt-4 flex-row items-center justify-between">
+        <NLWLogo />
+        <View className="flex-row gap-2">
+          <TouchableOpacity
+            onPress={logOut}
+            className="h-10 w-10 items-center justify-center rounded-full bg-red-500"
+          >
+            <Icon name="log-out" size={16} color="#000" />
+          </TouchableOpacity>
+
+          <Link href="/new" asChild>
+            <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-green-500">
+              <Icon name="plus" size={16} color="#000" />
+            </TouchableOpacity>
+          </Link>
+        </View>
+      </View>
+
+      <View className="mt-6 space-y-10">
+        {memories.map((memory) => {
+          return (
+            <View key={memory.id} className="space-y-4">
+              <View className="flex-row items-center gap-2">
+                <View className="h-px w-5 bg-gray-50"></View>
+                <Text className="font-body text-xs text-gray-100">
+                  {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY')}
+                </Text>
+              </View>
+              <View className="space-y-4">
+                <Image
+                  className="aspect-video w-full rounded-lg"
+                  source={{
+                    uri: memory.coverUrl,
+                  }}
+                  alt=""
+                />
+                <Text className="font-body text-base leading-relaxed text-gray-100">
+                  {memory.excerpt}
+                </Text>
+                <Link href="/memories/id" asChild>
+                  <TouchableOpacity
+                    activeOpacity={0.7}
+                    className="flex-row items-center gap-2"
+                  >
+                    <Text className="font-body text-sm text-gray-300">
+                      Ler mais
+                    </Text>
+                    <Icon name="arrow-right" size={16} color={'#727275'} />
+                  </TouchableOpacity>
+                </Link>
+              </View>
+            </View>
+          )
+        })}
+      </View>
+    </ScrollView>
   )
 }
